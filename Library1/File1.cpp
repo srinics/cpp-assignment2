481912f66d12ebbf1a6a1b1f8a206f82db367753
Srini R <srinics@hotmail.com>
2020-03-21 08:57:11 +0000

    Added lib1

---
+++ Library1/File1.cpp
@@ -0,0 +1,29 @@
+/*
+* Author: M1043833 ( Srinivasan Rajendran )
+* Description: This file (File1.cpp) contains simple functions to test dll load 
+*
+* */
+#include <iostream>
+#include "file1.h"
+using namespace std;
+
+
+/*
+Author: M1043833
+Function Name: Func1
+Description:
+Simple function just print the function function and it has no arguments and no return type
+*/
+void Func1(){
+	std::cout << "Calling Func1" << std::endl;
+}
+
+/*
+Author: M1043833
+Function Name: Func2
+Description:
+Simple function just print the function function and it has no arguments and no return type
+*/
+void Func2(){
+	std::cout << "Calling Func2" << std::endl;
+}
---
+++ Library1/File1.h
@@ -0,0 +1,13 @@
+/*
+Author: M1043833 ( Srinivasan Rajendran )
+* Description: This file (file1.h) contains contains prototype for file1.cpp
+*
+* */
+
+#ifndef __FILE1_H__
+#define __FILE1_H__
+
+void Func1();
+void Func2();
+
+#endif
